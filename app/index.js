85ae56330a9f6ab00fbfd52fe3646bd67dc0f97a
soila.torome <soila.torome@student.moringaschool.com>
2022-09-04 15:58:45 +0300

    Assign variables to selected classes

---
+++ app/index.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const packages = document.querySelector('.container .image-gallery');
+const carSelected = document.getElementById('car');
+const packageSelected = document.getElementById('package');
+const price = document.getElementById('price');
+const model = document.getElementById('model');
