76872e63a315a699f52ab68cb5fcd0cb964f4d88
soila.torome <soila.torome@student.moringaschool.com>
2022-09-05 07:45:55 +0300

    Create a function to calculate the total price

--- app/index.js
+++ app/index.js
@@ -1,6 +1,57 @@
 const container = document.querySelector('.container');
-const packages = document.querySelector('.container .image-gallery');
+const packages = document.querySelector('.container .image-gallery .overlay');
 const carSelected = document.getElementById('car');
-const packageSelected = document.getElementById('package');
+const package = document.getElementById('package');
 const price = document.getElementById('price');
 const model = document.getElementById('model');
+//console.log(model);
+const image = document.querySelector('.image');
+//console.log(image);
+
+
+
+const apiHost = "http://localhost:3000";
+let carPrice = +carSelected.value;
+console.log(typeof carPrice)
+
+function updateSelectedPackage() {
+    //const selectedPackage = document.querySelector('.container .image-gallery .image .overlay');
+    //const selectedPackage = document.querySelectorAll("li.image");
+    //const container = document.querySelector(".container");
+    //const selectedPackage = document.querySelectorAll("ul.image-gallery > li.image > div")
+    //const selectedPackage = seats.length;
+    //console.log(selectedPackage);
+    //for (let i = 0; i < packages.length; i++) {
+     //   console.log(`I ${packages[i]} and you`);
+     //   debugger;
+    //}
+
+    //return packages;
+
+
+
+    //const selectedPackageNumber =  selectedPackage.height;
+    //console.log(price);
+    let packagePrice = +image.value;
+    console.log(typeof packagePrice);
+
+    package.innerText = packagePrice;
+    price.innerText = packagePrice + carPrice;
+}
+
+
+
+container.addEventListener('click' , (e) => {
+    if(e.target.classList.contains('overlay')) {
+        e.target.classList.toggle('overlay');
+        //console.log(e.target);
+    }
+
+    updateSelectedPackage();
+});
+
+
+//packages.addEventListener("click" , totalPrice);
+//function totalPrice() {
+
+//}
